1c607f1d8ce8560b69f65934ec9e7755f0dd4c32
Federico T <ftedinur@itba.edu.ar>
2014-04-26 16:26:16 -0300

    queue inteface

--- src/server/msg_text_q.c
+++ src/server/msg_text_q.c
@@ -1,7 +1,46 @@
 #include "msg_text_q.h"
+#include "protocol.h"
 #include <string.h>
+#include <stdlib.h>
+
+typedef struct msg_node_t {
+	char text[CHT_MSG_SIZE];
+	struct msg_node_t *next;
+} msg_node;
 
 struct msg_text_q_t {
-	
+	msg_node *head;
+	msg_node *current;
+	msg_node *last;
 };
 
+msg_text_q *create_queue()
+{
+	msg_text_q *queue = malloc(sizeof(struct msg_text_q_t));
+	if (queue == NULL)
+	{
+		return NULL;
+	}
+
+	queue->head = NULL;
+	queue->last = NULL;
+	queue->current = NULL;
+
+	return queue;
+}
+
+int push_message(msg_text_q *queue, char *msg)
+{
+	if (queue->head == NULL)
+	{	}
+}
+
+void iter_reset(msg_text_q *queue)
+{
+	
+}
+
+char *iter_next(msg_text_q *queue)
+{
+	
+}
--- src/server/msg_text_q.h
+++ src/server/msg_text_q.h
@@ -1,6 +1,6 @@
 typedef struct msg_text_q_t msg_text_q;
 
 msg_text_q *create_queue();
-void push_message(msg_text_q *queue, char *msg);
+int push_message(msg_text_q *queue, char *msg);
 void iter_reset(msg_text_q *queue);
 char *iter_next(msg_text_q *queue);
