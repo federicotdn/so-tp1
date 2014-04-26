7c853bc0a4bee6a4d6b1b682dc15e28de2d1bb23
Federico T <ftedinur@itba.edu.ar>
2014-04-26 16:49:12 -0300

    msg_text_q firste edition

--- src/server/msg_text_q.c
+++ src/server/msg_text_q.c
@@ -12,6 +12,7 @@ struct msg_text_q_t {
 	msg_node *head;
 	msg_node *current;
 	msg_node *last;
+	int count;
 };
 
 msg_text_q *create_queue()
@@ -25,22 +26,64 @@ msg_text_q *create_queue()
 	queue->head = NULL;
 	queue->last = NULL;
 	queue->current = NULL;
+	queue->count = 0;
 
 	return queue;
 }
 
 int push_message(msg_text_q *queue, char *msg)
 {
-	if (queue->head == NULL)
-	{	}
+	if (queue->last == NULL)
+	{
+		msg_node *node = malloc(sizeof(msg_node));
+		if (node == NULL)
+		{
+			return -1;
+		}
+
+		queue->count = 1;
+		queue->head = node;
+		node->next = NULL;
+		strcpy(node->text, msg);
+		return 0;
+	}
+
+	msg_node *aux = malloc(sizeof(msg_node));
+	if (aux == NULL)
+	{
+		return -1;
+	}
+
+	queue->count++;
+	queue->last->next = aux;
+	queue->last = aux;
+	aux->next = NULL;
+	strcpy(aux->text, msg);
+
+	if (queue->count > CHT_HIST_SIZE)
+	{
+		aux = queue->head->next;
+		free(queue->head);
+		queue->head = aux;
+		queue->count--;
+	}
+
+	return 0;
 }
 
 void iter_reset(msg_text_q *queue)
 {
-	
+	queue->current = queue->head;
 }
 
 char *iter_next(msg_text_q *queue)
 {
-	
+	if (queue->current == NULL)
+	{
+		return NULL;
+	}
+
+	char *msg = queue->current->text;
+	queue->current = queue->current->next;
+	return msg;
 }
