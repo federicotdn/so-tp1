7ba80de501242bbb3a0678fd0bb0ba6d2da7f772
Federico T <ftedinur@itba.edu.ar>
2014-04-26 17:52:33 -0300

    include guards

--- src/server/msg_text_q.h
+++ src/server/msg_text_q.h
@@ -1,6 +1,11 @@
+#ifndef MSG_TEXT_Q_H
+#define MSG_TEXT_Q_H
+
 typedef struct msg_text_q_t msg_text_q;
 
 msg_text_q *create_queue();
 int push_message(msg_text_q *queue, char *msg);
 void iter_reset(msg_text_q *queue);
 char *iter_next(msg_text_q *queue);
+
+#endif
