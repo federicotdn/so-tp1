5eadad3d7b39c4b40cf80e49f4d0c571cf61b211
riveign <riveign@gmail.com>
2014-04-26 00:28:57 -0300

    sockets piola

--- src/server/server-remote.c
+++ src/server/server-remote.c
@@ -1,91 +1,72 @@
 #include <stdio.h>
-#include <sys/types.h> 
-#include <sys/socket.h>
 #include <netinet/in.h>
-
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 80
+#define PORT 43454
+#define SA struct sockaddr
+void func(int sockfd)
+{
+  char buff[MAX];
+  int n;
+  for(;;)
+  {
+    bzero(buff,MAX);
+    read(sockfd,buff,sizeof(buff));
+    printf("From client: %s\t To client : ",buff);
+    bzero(buff,MAX);
+    n=0;
+    while((buff[n++]=getchar())!='\n');
+    write(sockfd,buff,sizeof(buff));
+    if(strncmp("exit",buff,4)==0)
+    {
+      printf("Server Exit...\n");
+      break;
+    }
+  }
+}
 int init_server_remote()
 {
-  int sockfd, newsockfd, portno, clilen;
-  char buffer[256];
-  struct sockaddr_in serv_addr, cli_addr;
-  int  n;
-  pid_t pid;
-    /* First call to socket() function */
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0) 
+  int sockfd,connfd,len;
+  struct sockaddr_in servaddr,cli;
+  sockfd=socket(AF_INET,SOCK_STREAM,0);
+  if(sockfd==-1)
   {
-    perror("ERROR opening socket");
-    exit(1);
+    printf("socket creation failed...\n");
+    exit(0);
   }
-    /* Initialize socket structure */
-  bzero((char *) &serv_addr, sizeof(serv_addr));
-  portno = 5001;
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = INADDR_ANY;
-  serv_addr.sin_port = htons(portno);
-
-    /* Now bind the host address using bind() call.*/
-  if (bind(sockfd, (struct sockaddr *) &serv_addr,
-    sizeof(serv_addr)) < 0)
+  else
+    printf("Socket successfully created..\n");
+  bzero(&servaddr,sizeof(servaddr));
+  servaddr.sin_family=AF_INET;
+  servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+  servaddr.sin_port=htons(PORT);
+  if((bind(sockfd,(SA*)&servaddr, sizeof(servaddr)))!=0)
   {
-   perror("ERROR on binding");
-   exit(1);
+    printf("socket bind failed...\n");
+    exit(0);
   }
-    /* Now start listening for the clients, here 
-     * process will go in sleep mode and will wait 
-     * for the incoming connection
-     */
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     while (1) 
-     {
-      newsockfd = accept(sockfd, 
-        (struct sockaddr *) &cli_addr, &clilen);
-      if (newsockfd < 0)
-      {
-        perror("ERROR on accept");
-        exit(1);
-      }
-        /* Create child process */
-      pid = fork();
-      if (pid < 0)
-      {
-        perror("ERROR on fork");
-        exit(1);
-      }
-      if (pid == 0)  
-      {
-            /* This is the client process */
-        close(sockfd);
-        doprocessing(newsockfd);
-        exit(0);
-      }
-      else
-      {
-        close(newsockfd);
-      }
-    } /* end of while */
-
-}
-
-void doprocessing (int sock)
-{
-    int n;
-    char buffer[256];
-
-    bzero(buffer,256);
-
-    n = read(sock,buffer,255);
-    if (n < 0)
-    {
-        perror("ERROR reading from socket");
-        exit(1);
-    }
-    printf("Here is the message: %s\n",buffer);
-    n = write(sock,"I got your message",18);
-    if (n < 0) 
-    {
-        perror("ERROR writing to socket");
-        exit(1);
-    }
+  else
+    printf("Socket successfully binded..\n");
+  if((listen(sockfd,5))!=0)
+  {
+    printf("Listen failed...\n");
+    exit(0);
+  }
+  else
+    printf("Server listening..\n");
+  len=sizeof(cli);
+  connfd=accept(sockfd,(SA *)&cli,&len);
+  if(connfd<0)
+  {
+    printf("server acccept failed...\n");
+    exit(0);
+  }
+  else
+    printf("server acccept the client...\n");
+  func(connfd);
+  close(sockfd);
 }
